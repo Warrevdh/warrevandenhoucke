5e69526abd27ee432cfe9c961c9d6cc825ed75c7
Warre Vandenhoucke <73744852+Honingkoek@users.noreply.github.com>
2022-06-23 13:13:46 +0200

    mail

--- mail.php
+++ mail.php
@@ -1,10 +1,31 @@
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "warrevandenhoucke@hotmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+<?php  
+ 
+if(isset($_POST['submit'])) {
+ $mailto = "warrevandenhoucke@hotmail.com";  //My email address
+ //getting customer data
+ $name = $_POST['name']; //getting customer name
+ $fromEmail = $_POST['email']; //getting customer email
+ $subject = "Contact form"; //getting subject line from client
+ $subject2 = "Confirmation: Message was submitted successfully"; // For customer confirmation
+ 
+ //Email body I will receive
+ $message = "Name: " . $name . "\n"
+ . "Message: " . "\n" . $_POST['message'];
+ 
+ //Message for client confirmation
+ $message2 = "Dear" . $name . "\n"
+ . "Thank you for contacting me. We will get back to you shortly!" . "\n\n"
+ . "You submitted the following message: " . "\n" . $_POST['message'] . "\n\n"
+ . "Regards," . "\n";
+ 
+ //Email headers
+ $headers = "From: " . $fromEmail; // Client email, I will receive
+ $headers2 = "From: " . $mailto; // This will receive client
+ 
+ //PHP mailer function
+ 
+  $result1 = mail($mailto, $subject, $message, $headers); // This email sent to My address
+  $result2 = mail($fromEmail, $subject2, $message2, $headers2); //This confirmation email to client
+}
+
 ?>
