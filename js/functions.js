2d6c9bd33da1bd69c78966eb07882f0c91aa58d6
Warre Vandenhoucke <73744852+Honingkoek@users.noreply.github.com>
2022-06-23 13:38:39 +0200

    mail

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
 const submitBtn = document.getElementById("submitBtn");
 /*when clicking on submitbtn a text should appear under the button*/
-submitBtn.onclick(() => {
+submitBtn.addEventListener("click", function () {
   const text = document.getElementById("text");
   text.innerHTML = "Thank you for your submission";
 });
