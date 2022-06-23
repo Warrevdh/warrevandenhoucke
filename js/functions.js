8cb45a1e66fd62ee0315a31ed907adc2e14de8d8
Warre Vandenhoucke <73744852+Honingkoek@users.noreply.github.com>
2022-06-23 12:14:49 +0200

    mail

---
+++ js/functions.js
@@ -0,0 +1,9 @@
+your_email = "warrevandenhoucke@hotmail.com";
+emailSub = "Contact form";
+emailBody = document.getElementById("message").innerText;
+var email_btn = document.getElementById("contact-submit");
+email_btn.addEventListener("click", function (e) {
+  e.preventDefault();
+  location.href =
+    "mailto:" + your_email + "&subject=" + emailSub + "&body=" + emailBody;
+});
