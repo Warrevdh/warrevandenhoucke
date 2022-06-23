990fd49c5f214139bc0d687128828f24016e3378
Warre Vandenhoucke <73744852+Honingkoek@users.noreply.github.com>
2022-06-23 14:03:44 +0200

    mail

--- js/functions.js
+++ js/functions.js
@@ -4,3 +4,22 @@ submitBtn.addEventListener("click", function () {
   const text = document.getElementById("text");
   text.innerHTML = "Thank you for your submission";
 });
+
+const form = document.getElementById("form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  let formData = new FormData(form);
+  fetch("/", {
+    method: "POST",
+    headers: { "Content-Type": "multipart/form-data" },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then(() => ntlFormSuccess())
+    .catch((error) => alert(error));
+});
+
+function ntlFormSuccess() {
+  console.log("form submitted");
+  form.reset();
+  document.querySelector(".successNoti").style.display = "inline";
+}
