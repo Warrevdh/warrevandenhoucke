23f75c3e10f26c8825db92e73782bac74cc52bad
Warre Vandenhoucke <73744852+Honingkoek@users.noreply.github.com>
2022-06-23 14:07:01 +0200

    mail

--- js/functions.js
+++ js/functions.js
@@ -1,10 +1,3 @@
-const submitBtn = document.getElementById("submitBtn");
-/*when clicking on submitbtn a text should appear under the button*/
-submitBtn.addEventListener("click", function () {
-  const text = document.getElementById("text");
-  text.innerHTML = "Thank you for your submission";
-});
-
 const form = document.getElementById("form");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -13,13 +6,11 @@ form.addEventListener("submit", (event) => {
     method: "POST",
     headers: { "Content-Type": "multipart/form-data" },
     body: new URLSearchParams(formData).toString(),
-  })
-    .then(() => ntlFormSuccess())
-    .catch((error) => alert(error));
+  }).then(() => ntlFormSuccess());
 });
 
 function ntlFormSuccess() {
-  console.log("form submitted");
+  const text = document.getElementById("text");
+  text.innerHTML = "Thank you for your submission";
   form.reset();
-  document.querySelector(".successNoti").style.display = "inline";
 }
